d64867729bc5291f2f9871d055a26490220d3dd8
KL <KL@172.19.5.177>
2016-02-08 11:17:09 +0100

    entity Song default title ONE LOVE
    Signed-off-by: KL <KL@172.19.5.177>

---
+++ lundi/lundi-ejb/src/main/java/edu/esprit/lundi/persistence/Song.java
@@ -0,0 +1,47 @@
+package edu.esprit.lundi.persistence;
+
+import java.io.Serializable;
+import java.lang.Integer;
+import java.lang.String;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Song
+ *
+ */
+@Entity
+
+public class Song implements Serializable {
+
+	   
+	@Id
+	private Integer id;
+	private String title;
+
+	public Song() {
+	}   
+	
+	
+	public Song(Integer id, String title) {
+		super();
+		this.id = id;
+		this.title = "One Love";
+	}
+
+
+	public Integer getId() {
+		return this.id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}   
+	public String getTitle() {
+		return this.title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+   
+}
